0bff838fbf44f72692298ba8ee543a312c911f8d
Marco <marco.silva@querointernet.net>
2021-08-09 12:09:28 -0300

    db e env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health/health.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRoot(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }),
+    UsersModule, 
+    TerminusModule
+],
+  controllers: [HealthController],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/health/health.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Dependencies, Get } from '@nestjs/common';
+import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+
+@Controller('health')
+@Dependencies(HealthCheckService, HttpHealthIndicator)
+
+export class HealthController {
+    constructor(
+        private health,
+        private http,
+    ) {}
+
+    @Get()
+    @HealthCheck()
+    healthCheck() {
+        return this.health.check([
+            async () => this.http.pingCheck('local', 'http://localhost:3000'),
+        ])
+    }
+}
